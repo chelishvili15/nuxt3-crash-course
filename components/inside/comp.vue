0e0053f63f0f8677d3e45172496b8f70d6f5315b
chelishvili15 <chelo@redberry.ge>
2023-08-07 17:02:02 +0400

    lesson2: data fetching

---
+++ components/inside/comp.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>inside component</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,49 @@
 <template>
-    <Title> Main page </Title>
     <div>
-        <Counter />
-    </div>
+        <Title> Main page </Title>
+        <div>
+            <Counter />
+            <InsideComp />
+        </div>
 
-    <div>
-        x: {{ x }}
-        y: {{ y }}
+        <div>
+            x: {{ x }}
+            y: {{ y }}
+        </div>
+
+        <div>
+            <ul>
+                <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+            </ul>
+        </div>
+
+        <div>
+            <ul>
+                <li v-for="user in users2" :key="user.id">{{ user.id }}</li>
+            </ul>
+        </div>
+
+        <pre>
+            {{ user }}
+        </pre>
     </div>
 </template>
 
 <script setup>
+const users = ref(['aaaa'])
+
 const {x, y} = useMouse()
+
+onMounted(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => users.value = data)
+})
+
+const { data: users2 } = await useFetch('https://jsonplaceholder.typicode.com/users')
+
+const { data: user } = await useFetch('https://jsonplaceholder.typicode.com/users/1', { pick: ['id', 'name', 'email'] }) 
+
 </script>
 
 <style lang="scss" scoped>
